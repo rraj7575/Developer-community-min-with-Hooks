49f4e49f19f7170ef9198ea179c4dcc705c05968
rohit <rohit.raj@appstreet.io>
2018-07-09 20:34:20 +0530

    added keys for dev and production

---
+++ config/key_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoAwsURL: process.env.MONGO_URL,
+  secretKey: process.env.SECREAC_OR_KEY,
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoAwsURL: 'mongodb://rohit:abcd1234@ds033113.mlab.com:33113/connect-world',
+  secretKey: 'secret'
+}
