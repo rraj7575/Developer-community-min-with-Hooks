8b2733acdc1c222e2c156ba97ed12dc98f6e2f63
rohit <rohit.raj@appstreet.io>
2018-07-10 15:31:27 +0530

    change in script package.json

--- server.js
+++ server.js
@@ -29,15 +29,17 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/posts', posts)
 app.use('/api/profile', profile)
-
 //Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use( express.static('client/build'))
+  console.log(process.env.NODE_ENV)
+  console.log('in production')
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__filename, 'client', 'build', 'index.html'))
   })
 }
+
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`))
