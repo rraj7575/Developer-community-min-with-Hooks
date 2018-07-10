d1bb7b937d9efa0bf048e00e8f6be92d4b36659b
rohit <rohit.raj@appstreet.io>
2018-07-10 15:03:47 +0530

    removed port on client side

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.use('/api/posts', posts)
 app.use('/api/profile', profile)
 
 //Server static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   //Set static folder
-//   app.use('client/build')
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__filename, 'client', 'build', 'index.html'))
-//   })
-// }
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use( express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__filename, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`))
