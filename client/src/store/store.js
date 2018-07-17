424bf16eda2c59971afa4269e63109980c27fa8d
rohit <rohit.raj@appstreet.io>
2018-07-17 11:23:53 +0530

    remove redux compose fuction from store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,18 +3,18 @@ import thunk from 'redux-thunk'
 import rootReducer from './../reducers/index'
 const middleware = [thunk]
 const initialState = {}
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//     applyMiddleware(...middleware)
-//   )
-// )
-
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
+  )
 )
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   applyMiddleware(...middleware)
+// )
  export default store
